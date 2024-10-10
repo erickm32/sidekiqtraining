f1ae444b67984592ddc7ae0ddb7fd354ba025811
Érick Moreira <erick@sementesrastreadas.com.br>
2024-10-10 15:57:14 -0300

    Also using shoulda_matchers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -71,3 +71,10 @@ RSpec.configure do |config|
 
   FactoryBot.find_definitions
 end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
